1fb2caa8d0c3d296768ea71e82a2ccbaf55b5d21
Michael Prather <mprather1@gmail.com>
2017-07-13 04:07:30 +0000

    refactored directories

--- roles/nginx/files/certbot.sh
+++ roles/nginx/files/certbot.sh
@@ -4,5 +4,5 @@ docker run -it --name certbot \
   -v "/home/core/certbot/data/etc:/etc/letsencrypt" \
   -v "/home/core/certbot/data/var-lib:/var/lib/letsencrypt" \
   -v "/home/core/certbot/data/webroot:/webroot" \
-  certbot/certbot certonly --webroot -w /webroot -d $DOMAIN --email $EMAIL --agree-tos -n --dry-run
+  certbot/certbot certonly --webroot -w /webroot -d $DOMAIN --email $EMAIL --agree-tos -n
 
